9c1675630001e5773212ba3ff5d86ed84f4bba9d
mauor <mauorozcoo@gmail.com>
2024-04-10 13:54:29 -0600

    Add yaml filee

--- src/config/plugins/envs.plugin.ts
+++ src/config/plugins/envs.plugin.ts
@@ -6,5 +6,9 @@ export const envs = {
     MAILER_SERVICE: env.get('MAILER_SERVICE').required().asString(),
     MAILER_EMAIL: env.get('MAILER_EMAIL').required().asEmailString(),
     MAILER_KEY_SECRET: env.get('MAILER_KEY_SECRET').required().asString(),
-    PROD: env.get('PROD').required().asBool()
+    PROD: env.get('PROD').required().asBool(),
+    MONGO_URL: env.get('MONGO_URL').required().asString(),
+    MONGO_DB_NAME: env.get('MONGO_DB_NAME').required().asString(),
+    MONGO_USER: env.get('MONGO_USER').required().asString(),
+    MONGO_PASSWORD: env.get('MONGO_PASSWORD').required().asString(),
 }
